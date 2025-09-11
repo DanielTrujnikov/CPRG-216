e1eeda16269374ec7873abfa44adb744ff1931c2
Daniel <trujnd@gmail.com>
2025-09-11 17:41:00 -0600

    CPRG-216

---
+++ demo3.py
@@ -0,0 +1,19 @@
+x = 6.7
+y = 7
+z = x+y
+aura = "67 diddy blud" 
+mango = "41 phonk"
+sigma = True
+beta = False
+
+print(z)
+print(aura)
+print(mango)
+print(sigma)
+print(beta)
+
+text_1 = 5
+msg_1 = "Bonjoir"
+msg_2 = "Bon Voyage"
+
+print(msg_2)
---
+++ demo4.py
@@ -0,0 +1,12 @@
+# naming a variable
+# start with a letter
+# you can include digits, underscores and letters
+# can't start with a digit
+# a convention a traditional way of doing something
+# there is a convention for python names of variables
+
+book_id = 1234
+cat_colour = "red"
+greeting_msg = "hello"
+
+fsbkj_374 = 4 # not following convention
