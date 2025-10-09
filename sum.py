f7ee5e080cfbd3c9514d2b24be57b5a32c8a7e2e
Daniel <trujnd@gmail.com>
2025-10-09 15:05:17 -0600

    week4.3

---
+++ sum.py
@@ -0,0 +1,21 @@
+'''
+number = 0
+sum = 0
+
+while number < 100:
+    number += 1
+    sum += number
+    print(number)
+
+print("The sum is", sum)
+'''
+
+number = 0
+factorial = 1
+
+while number < 100:
+    number += 1
+    factorial *= number
+    print(number)
+
+print("The factorial is", factorial)
